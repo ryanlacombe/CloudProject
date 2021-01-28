ed1def6ed139641650563e149627fc7a8095abf3
Rain <ryanalacombe@gmail.com>
2021-01-28 14:14:32 -0600

    Blueprint Only Voronoi Demo

--- Source/ComputeShaders/ComputeShaders.cpp
+++ Source/ComputeShaders/ComputeShaders.cpp
@@ -5,6 +5,10 @@ DEFINE_LOG_CATEGORY(ComputeShaders);
 void FComputeShaders::StartupModule()
 {
 	UE_LOG(ComputeShaders, Warning, TEXT("Compute Shader module has started!"));
+	
+	//Finds the shaders directory for use
+	//FString ShaderDirectory = FPaths::Combine(FPaths::ProjectDir(), TEXT("Shaders/Private"));
+	//AddShaderSourceDirectoryMapping("/CustomShaders", ShaderDirectory);
 }
 
 void FComputeShaders::ShutdownModule()
